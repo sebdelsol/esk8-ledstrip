7f02f652063c76936ecb48a5bc1397b1364af42a
sebdelsol <seb.morin@gmail.com>
2020-03-01 01:40:13 +0100

    clean

--- include/myPins.h
+++ include/myPins.h
@@ -1,11 +1,10 @@
 #pragma once
 
-// I2C by default:
 #define SDA           21
 #define SCL           22
-#define LDR_PIN       35//33 //12 ==> reset ?
-#define LED_PIN       14//33//32
+#define LDR_PIN       35
+#define LED_PIN       14
 #define LEDR_PIN      12
 #define LEDF_PIN      13
-#define BUTTON_PIN    17//14
-#define LIGHT_PIN     15//13//25
+#define BUTTON_PIN    17
+#define LIGHT_PIN     15
