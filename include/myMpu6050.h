11a21640ecb9d81752043de8d41797b94216546b
sebdelsol <seb.morin@gmail.com>
2020-03-07 16:51:56 +0100

    clean

--- include/myMpu6050.h
+++ include/myMpu6050.h
@@ -6,22 +6,23 @@
 #include <myPins.h>
 #include <helper_3dmath.h>
 
-#define ACCEL_AVG .05 //.1
-#define ACCEL_BASE_FREQ 60.
+//----------------------------- smooth accel & gyro
+#define ACCEL_AVG       .05 // use 5% of the new measure in the avg
+#define ACCEL_BASE_FREQ 60. // based on a 60fps measure
 
 //----------------------------- OFFSETS
 
-// #define MPU_ZERO // if you want to compute the offset
+// #define MPU_ZERO // if you need to compute the offset
 #ifdef MPU_ZERO
   void MPUzero(Stream &serial, void (*handleOta)());
 #endif
 
-#define XGyroOffset 80 //77
-#define YGyroOffset 4
-#define ZGyroOffset 8
-#define XAccelOffset -1843 // -1772
-#define YAccelOffset -267 // -155
-#define ZAccelOffset 1297 // 1270
+#define XGyroOffset   80 //77
+#define YGyroOffset   4
+#define ZGyroOffset   8
+#define XAccelOffset  -1843 // -1772
+#define YAccelOffset  -267 // -155
+#define ZAccelOffset  1297 // 1270
 
 //-----------------------------
 #define TOdeg(x) ( x * 180/M_PI )
