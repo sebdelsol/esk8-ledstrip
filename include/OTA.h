e853e04f3bb6313c19e8d5cb54ccb40464ede681
sebdelsol <seb.morin@gmail.com>
2020-03-27 13:30:12 +0100

    clean OTA

--- include/OTA.h
+++ include/OTA.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define NODEBUG_WEBSOCKETS
+#define OTA_HOSTNAME "esk8" // see platformio.ini where >> upload_port = "esk8.local"
 
 class OTA
 {
--- src/OTA.cpp
+++ src/OTA.cpp
@@ -12,34 +12,20 @@ void OTA::begin()
   {
     mOn = true;
     Serial << "OTA Connected, IP address: " << WiFi.localIP() << endl;
-
-    // ArduinoOTA.setPort(3232); // already default port
-    ArduinoOTA.setHostname("esk8");
+    ArduinoOTA.setHostname(OTA_HOSTNAME);
 
     ArduinoOTA
       .onStart([]() {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH)
-          type = "sketch";
-        else // U_SPIFFS
-          type = "filesystem";
-
-        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-        Serial.println("Start updating " + type);
+        Serial << "Start updating " << (ArduinoOTA.getCommand() == U_FLASH ? "sketch" : "filesystem") << endl;
       })
       .onEnd([]() {
-        Serial.println("\nEnd");
+        Serial << "\nEnd\n";
       })
       .onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+        Serial << "Progress: " << (progress / (total / 100)) << "%\r";
       })
       .onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR) Serial.println("End Failed");
+        Serial << "Error " << error << endl; // check ArduinoOTA.h for errors
       });
 
     ArduinoOTA.begin();
