a57828546f47270892b52da91df87a22ca3a3874
sebdelsol <seb.morin@gmail.com>
2020-08-09 16:15:13 +0200

    simpler

--- include/VarMacro.h
+++ include/VarMacro.h
@@ -35,8 +35,7 @@ inline Print& JoinbySpace(Print& stream) { return stream; }
 inline Print& JoinbySpace(Print& stream, T arg) { return stream << arg; }
 template<class T, class... Args> inline Print& JoinbySpace(Print& stream, T first, Args... args) 
 { 
-  stream << first << " ";
-  return JoinbySpace(stream, args...); 
+  return stream << first << " " << JoinbySpace(stream, args...); 
 }
 
 // -----------------------------------------------------
