53dab4e6028378c90f22914758580d243abcc262
sebdelsol <seb.morin@gmail.com>
2020-08-12 12:20:40 +0200

    clean

--- include/Variadic.h
+++ include/Variadic.h
@@ -20,28 +20,32 @@
 #define ForEachMacro(_m, _p, ...) _Mn(__VA_ARGS__)(_m, _p, __VA_ARGS__)
 
 // -----------------------------------------------------
-// create JoinbySpace(stream, ...) at least 1+2 args
+// create JoinbySpace(stream, ...) 
+
+inline Print& JoinbySpace(Print& stream) {}
 
 template<class Last> 
-  inline Print& JoinbySpace(Print& stream, Last last) 
-  { return stream << last; }
+inline Print& JoinbySpace(Print& stream, Last last) { return stream << last; }
+
 template<class First, class... Args> 
-  inline Print& JoinbySpace(Print& stream, First first, Args... args) 
-  { 
-    stream << first << " ";
-    return JoinbySpace(stream, args...); 
-  }
+inline Print& JoinbySpace(Print& stream, First first, Args... args) 
+{ 
+  stream << first << " ";
+  return JoinbySpace(stream, args...); 
+}
 
 // -----------------------------------------------------
-// create the _forEach(...) method that calls _method(arg) for each arg
+// create the _methods(...) that calls _method(arg) for each arg
 
-#define _forEachName(_method) _method##s
+#define _forEach(_method) _method##s
 
-#define ForEachMethod(_method)                                      \
-    inline void _forEachName(_method)() {};                         \
-  template<class First, class... Args>                              \
-    inline void _forEachName(_method)(First& first, Args&... args)  \
-    {                                                               \
-      _method(first);                                               \
-      _forEachName(_method)(args...);                               \
-    };
+#define ForEachMethod(_method)                                \
+                                                              \
+  inline void _forEach(_method)() {};                         \
+                                                              \
+  template<class First, class... Args>                        \
+  inline void _forEach(_method)(First& first, Args&... args)  \
+  {                                                           \
+    _method(first);                                           \
+    _forEach(_method)(args...);                               \
+  };
