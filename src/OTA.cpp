932a96c536487978c14807716213e0b025644022
sebdelsol <seb.morin@gmail.com>
2020-09-25 22:32:13 +0200

    Update OTA.cpp

--- src/OTA.cpp
+++ src/OTA.cpp
@@ -5,10 +5,15 @@ void OTA::begin()
 {
   if(WiFi.status() == WL_CONNECTED)
   {
-    // OTA_NAME & OTA_PORT are shared by OTA and the socket server, check platformio build_flags
+    // OTA_NAME & OTA_PORT are shared by OTA and the socket server, 
+    // check platformio build_flags
     _log << "OTA started, answer @ " << OTA_NAME << ".local:" << OTA_PORT << endl;
-    ArduinoOTA.setHostname(OTA_NAME).setPort(OTA_PORT).setMdnsEnabled(!MDNS.begin(OTA_NAME)); 
-    ArduinoOTA.begin();
+    
+    ArduinoOTA.setHostname(OTA_NAME)
+              .setPort(OTA_PORT)
+              .setMdnsEnabled(!MDNS.begin(OTA_NAME)) 
+              .begin();
+    
     mBegun = true;
   }
 }
